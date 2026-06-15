e4df8b32ef674a7f738e27b241e5b86623d4c86f
danielsantosalmeida77-dev <danielsantosalmeida77@gmail.com>
2026-06-15 17:19:11 -0300

    Delete Módulo3 directory

--- Módulo3/Aula 3.2 - Relacionais e Lógicas - Exercícios/aula2_questao1.py
+++
@@ -1,4 +0,0 @@
-juliana = int(input("Digite sua idade"))
-Cris = int(input("Digite sua idade"))
-
-print( juliana > 17 and Cris > 17)
--- Módulo3/Aula 3.2 - Relacionais e Lógicas - Exercícios/aula2_questao2.py
+++
@@ -1,4 +0,0 @@
-juliana = int(input("Digite sua idade"))
-Cris = int(input("Digite sua idade"))
-
-print( juliana > 17 or Cris > 17)
